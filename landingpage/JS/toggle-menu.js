9e9cf74d37340795fff9a551b4b699feaf78c5d0
alevtinabaranetcka <alevtinabaranecka@gmail.com>
2017-06-20 22:59:14 +0300

    js

---
+++ landingpage/JS/toggle-menu.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+} 
+
+function myFunction1() {
+    var x = document.getElementById('myDIVlinks');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+} 
+
+function myFunction2() {
+    var x = document.getElementById('myDIVabout');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+} 
